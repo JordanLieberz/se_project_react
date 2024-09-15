bed0d77aee6f3b73edb1694a54eae34db3c3a47d
JordanLieberz <Jordan.t.lieberz@gmail.com>
2024-09-15 17:49:11 -0400

    Add & Style Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,12 @@
+import "./Footer.css";
+
 function Footer() {
-  <div className="footer">
-    <h2>Developed by Jordan Lieberz</h2>
-    <p>2024</p>
-  </div>;
+  return (
+    <div className="footer">
+      <h2 className="footer__developer">
+        Developed by Jordan Lieberz <span>2024</span>
+      </h2>
+    </div>
+  );
 }
 export default Footer;
