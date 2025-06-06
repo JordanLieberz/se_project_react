d32a1f2c828133f1a2a11ab985ab31a9577fed77
JordanLieberz <Jordan.t.lieberz@gmail.com>
2025-06-06 12:53:29 -0400

    update avatar and name using modal

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,11 +1,19 @@
-import avatar from "../../assets/avatar.png";
+import React, { useContext } from "react";
+import avatarPlaceholder from "../../assets/avatar.png";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 import "./SideBar.css";
 
-function SideBar(activeModal, handleEditProfileClick, onSignOut) {
+function SideBar({ handleEditProfileClick, onSignOut }) {
+  const currentUser = useContext(CurrentUserContext);
+
   return (
     <div className="sidebar">
-      <img className="sidebar__avatar" src={avatar} alt="Default Avatar" />
-      <p className="sidebar__username">Username</p>
+      <img
+        className="sidebar__avatar"
+        src={currentUser?.avatar || avatarPlaceholder}
+        alt="User Avatar"
+      />
+      <p className="sidebar__username">{currentUser?.name || "Username"}</p>
     </div>
   );
 }
