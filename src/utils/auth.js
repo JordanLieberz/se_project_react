df935ce502766014b933a8da8d77c3b2c91407f4
JordanLieberz <Jordan.t.lieberz@gmail.com>
2025-12-30 09:52:59 -0500

    changes

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://api.wtwr99.justlearning.net/"
+    ? "https://api.wtwr99.justlearning.net"
     : "http://localhost:3001";
 import { checkResponse } from "./api";
 
